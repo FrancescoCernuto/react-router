a21e7c4a84c0fdfd03abb9f2e46e3fd1b5bed768
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-10 15:58:08 +0100

    add function for layout

--- src/assets/layout/DefaultLayout.jsx
+++ src/assets/layout/DefaultLayout.jsx
@@ -1,2 +1,18 @@
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+
+export default function defaultLayout() {
+    return (
+        <>
+            <div className="wrapper" data-bs-theme="dark">
+                <header>
+                    <Navbar />
+                </header>
+                <main className="container p-3">
+                    <Outlet />
+                </main>
+                <footer></footer>
+            </div>
+        </>
+    );
+}
