1ac444e511b05482ef99534b8122878c7a3437dd
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-10 15:47:53 +0100

    Add NavBar

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,1 +1,18 @@
 import { Link, NavLink } from "react-router-dom";
+
+export default function Navbar() {
+    return (
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Posts</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+
+        </ul>
+    );
+}
