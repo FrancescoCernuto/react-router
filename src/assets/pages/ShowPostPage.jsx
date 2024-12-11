20a921d8fa4e6ed86214af64309107701280616e
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-11 16:17:00 +0100

    add url

--- src/assets/pages/ShowPostPage.jsx
+++ src/assets/pages/ShowPostPage.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-export default function ShowPostPage()
+export default function ShowPostPage() {
+    const { id: postId } = useParams();
+}
