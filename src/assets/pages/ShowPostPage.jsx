0dd4febb4ca403123a9e38ae0671518465305bd2
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-12-11 16:30:59 +0100

    add api request postid statement

--- src/assets/pages/ShowPostPage.jsx
+++ src/assets/pages/ShowPostPage.jsx
@@ -6,3 +6,5 @@ export default function ShowPostPage() {
     const [post, setPost] = useState(null)
 }
 const indexUrl = import.meta.env.VITE_POSTS_INDEX;
+function fetchPost(postId) {
+    fetch(indexUrl + `/${postId}`)
